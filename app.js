cc881739a46078b4a966d13e96ecbadb2bd3529f
lodoisambuu <lodoisambuu0102@gmail.com>
2023-01-17 12:04:43 +0800

    togloom urgeljlel

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-var activePlayer = 1;
+var activePlayer = 0;
 
 var scores = [0, 0];
 
 var roundScore = 0;
-
-var dice = Math.floor(Math.random() * 6 + 1);
 
 // dom objectig querySelector gsen funktseer haina.
 // window.document.querySelector("#score-0").textContent = dice;
@@ -13,8 +11,44 @@ var dice = Math.floor(Math.random() * 6 + 1);
 // document.querySelector("#score-0").innerHTML = "<em>YES!</em>";
 
 // program ehlel
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+
+// DOM mod butetsees getElementById() ni querySelector() hamaagu iluu hurdan haidag.
+// querySelector dotor haihda #score-0 gej haij bsan bol id aar ni haij bga uyed teghgu
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
+
+var diceDom = document.querySelector(".dice");
+
+diceDom.style.display = "none";
+
+// eventinha nerig argument dotor bichij ugnu, hajuud ni tuhain hiigdeh funktsig bichij ugnu
+document.querySelector(".btn-roll").addEventListener("click", shooShid);
+
+// ene funktsig anynemous buyu nergu funkts bolgoj bolno.
+function shooShid(){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+
+    if(diceNumber !== 1){
+        roundScore = roundScore + diceNumber;
+        document.getElementById("current-" + activePlayer).textContent = roundScore;
+    } else {
+        // shoo 1 buusan uyed
+        roundScore = 0;
+        document.getElementById("current-" + activePlayer).textContent = 0;
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+        // olon classuudas active gsen classig hasaj ulaan tsegiig shiljuuleh
+        // document.querySelector(".player-0-panel").classList.remove("active");
+        // document.querySelector(".player-1-panel").classList.add("active");
+        // toggle funkts ni tuhain class bval nemne bhgu bol ustgana.
+        document.querySelector(".player-0-panel").classList.toggle("active");
+        document.querySelector(".player-1-panel").classList.toggle("active");
+
+        diceDom.style.display = "none";
+    }
+}
